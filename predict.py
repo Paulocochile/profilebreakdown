c76bb4e07d1932f3dc37a4c596517f532b78eb8b
Paulocochile <63756183+Paulocochile@users.noreply.github.com>
2020-10-28 21:31:14 +0000

    Removing Model ID

--- predict.py
+++ predict.py
@@ -12,19 +12,19 @@ project_location = f"projects/profilebreakdown/locations/eu"
 
 
 project_id = "profilebreakdown"
-model_id = "TCN7649583869414342656"
+model_id = "XXXXXXXXXXXXXXXXXXXXX"
 content = "Coronavirus will be solved very soon, no more people will die"
 
 prediction_client = automl.PredictionServiceClient(client_options=client_options)
 
 # Get the full path of the model.
-model_full_id = "projects/1047356004045/locations/eu/models/TCN7649583869414342656"
+model_full_id = "projects/XXXXXXXX/locations/eu/models/XXXXXXXXXXXXXXXXXXX"
 
 # Supported mime_types: 'text/plain', 'text/html'
 # https://cloud.google.com/automl/docs/reference/rpc/google.cloud.automl.v1#textsnippet
 
 
-        
+
 def my_function():
     text_snippet = automl.TextSnippet(
         content=content, mime_type="text/plain"
@@ -37,7 +37,7 @@ def my_function():
         if annotation_payload.classification.score>0.5:
             row.append(annotation_payload.display_name)
     writer.writerow(row)
-        
+
 with open('test.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile)
     writer = csv.writer(open('output.csv', 'w'))
